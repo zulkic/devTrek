6756be86651b433510d2603964fb8b60e8ad5cc7
Nicolas <negs.hunterx@gmail.com>
2015-05-12 14:09:17 -0300

    12-05-2015 Spinner con iconos

--- app/src/main/java/com/mapas/franciscojavier/trekkingroute/DetallesRuta.java
+++ app/src/main/java/com/mapas/franciscojavier/trekkingroute/DetallesRuta.java
@@ -4,6 +4,7 @@ package com.mapas.franciscojavier.trekkingroute;
  * Created by nicolas on 10-05-2015.
  */
 
+import android.content.Context;
 import android.os.Bundle;
 import android.app.Fragment;
 import android.view.LayoutInflater;
@@ -11,6 +12,7 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.ImageView;
 import android.widget.TextView;
 import android.widget.Toast;
 
@@ -18,7 +20,6 @@ import java.util.ArrayList;
 import java.util.List;
 import android.widget.ArrayAdapter;
 import android.widget.Spinner;
-import android.widget.Toast;
 
 
 public class DetallesRuta extends Fragment implements View.OnClickListener{
@@ -28,8 +29,17 @@ public class DetallesRuta extends Fragment implements View.OnClickListener{
     private static final String ARG_PARAM1 = "nada";
     private static final String ARG_PARAM2 = "vacio";
     private static String ARG_PARAM3 = "hollow";
-    private Spinner spinner1;
+    private Spinner spinnerReco;
     private Button btnSubmit;
+    private String Caminando="\nCaminando";
+    private String Trotando="\nTrotando";
+    private String Corriendo="\nCorriendo";
+    private String Bicicleta="\nBicicleta";
+    private String Caballo="\nCaballo";
+    private String Auto="\nAuto";
+
+    String[] listRecorido = {Caminando, Trotando, Corriendo,Bicicleta, Caballo, Auto};
+
     public DetallesRuta() {
         // Required empty public constructor
     }
@@ -58,22 +68,21 @@ public class DetallesRuta extends Fragment implements View.OnClickListener{
         textFragment.setOnClickListener(this);
 
         //SPINER
-        spinner1 = (Spinner) v.findViewById(R.id.spinner_recorrido);
-        List<String> list = new ArrayList<String>();
-        list.add("Caminando");
-        list.add("Trotando");
-        list.add("Corriendo");
-        list.add("En Bicicleta");
-        list.add("A Caballo");
-        list.add("En Auto");
+        spinnerReco = (Spinner) v.findViewById(R.id.spinner_recorrido);
+        /**List<String> listRecorido = new ArrayList<String>();
+        listRecorido.add("Caminando");
+        listRecorido.add("Trotando");
+        listRecorido.add("Corriendo");
+        listRecorido.add("En Bicicleta");
+        listRecorido.add("A Caballo");
+        listRecorido.add("En Auto");*/
 
-        ArrayAdapter<String> dataAdapter = new ArrayAdapter<String>
-                (getActivity(), android.R.layout.simple_spinner_item,list);
+        //ArrayAdapter<String> dataAdapter = new ArrayAdapter<String>(getActivity(), android.R.layout.simple_spinner_item,listRecorido);
 
-        dataAdapter.setDropDownViewResource
-                (android.R.layout.simple_spinner_dropdown_item);
+        //dataAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
 
-        spinner1.setAdapter(dataAdapter);
+        //spinnerReco.setAdapter(dataAdapter);
+        spinnerReco.setAdapter(new MyCustomAdapter(getActivity(), R.layout.row, listRecorido));
 
         // Spinner item selection Listener
         addListenerOnSpinnerItemSelection();
@@ -88,7 +97,7 @@ public class DetallesRuta extends Fragment implements View.OnClickListener{
             case R.id.button_guardar_ruta:
                 EditText e = (EditText) getActivity().findViewById(R.id.editText_nombre_ruta);
                 TextView textFragment = (TextView)getActivity().findViewById(R.id.editText_tiempo_estimado);
-                spinner1 = (Spinner) getActivity().findViewById(R.id.spinner_recorrido);
+                spinnerReco = (Spinner) getActivity().findViewById(R.id.spinner_recorrido);
                 if(e.getText().length()<LARGO_NOMBRE_RUTA){
                     Toast.makeText(getActivity().getBaseContext(), "Ingrese un Nombre", Toast.LENGTH_SHORT).show();
                 }
@@ -98,7 +107,7 @@ public class DetallesRuta extends Fragment implements View.OnClickListener{
                             "Datos: "+"\n"
                                    + e.getText()+"\n"
                                    + textFragment.getText()+"\n"
-                                   + String.valueOf(spinner1.getSelectedItem())
+                                   + String.valueOf(spinnerReco.getSelectedItem())
                             ,Toast.LENGTH_SHORT).show();
                 }
                 break;
@@ -109,7 +118,65 @@ public class DetallesRuta extends Fragment implements View.OnClickListener{
     // Add spinner data
     public void addListenerOnSpinnerItemSelection(){
 
-        spinner1.setOnItemSelectedListener(new CustomOnItemSelectedListener());
+        spinnerReco.setOnItemSelectedListener(new CustomOnItemSelectedListener());
+    }
+
+    public class MyCustomAdapter extends ArrayAdapter<String>{
+
+        public MyCustomAdapter(Context context, int textViewResourceId,
+                               String[] objects) {
+            super(context, textViewResourceId, objects);
+            // TODO Auto-generated constructor stub
+        }
+
+        @Override
+        public View getDropDownView(int position, View convertView,
+                                    ViewGroup parent) {
+            // TODO Auto-generated method stub
+            return getCustomView(position, convertView, parent);
+        }
+
+        @Override
+        public View getView(int position, View convertView, ViewGroup parent) {
+            // TODO Auto-generated method stub
+            return getCustomView(position, convertView, parent);
+        }
+
+        public View getCustomView(int position, View convertView, ViewGroup parent) {
+            // TODO Auto-generated method stub
+            //return super.getView(position, convertView, parent);
+
+            LayoutInflater inflater=getActivity().getLayoutInflater();
+            View row=inflater.inflate(R.layout.row, parent, false);
+            TextView label=(TextView)row.findViewById(R.id.tipo_recorrido);
+            label.setText(listRecorido[position]);
+
+            ImageView icon=(ImageView)row.findViewById(R.id.icon);
+
+            if (listRecorido[position]==Caminando){
+                icon.setImageResource(R.drawable.ic_caminando);
+            }
+            else if (listRecorido[position]==Trotando){
+                icon.setImageResource(R.drawable.ic_trotando);
+            }
+            else if (listRecorido[position]==Corriendo){
+                icon.setImageResource(R.drawable.ic_corriendo);
+            }
+            else if (listRecorido[position]==Bicicleta){
+                icon.setImageResource(R.drawable.ic_bicicleta);
+            }
+            else if (listRecorido[position]==Caballo){
+                icon.setImageResource(R.drawable.ic_caballo);
+            }
+            else if (listRecorido[position]==Auto){
+                icon.setImageResource(R.drawable.ic_auto);
+            }
+            else{
+                icon.setImageResource(R.drawable.ic_droid);
+            }
+
+            return row;
+        }
     }
 
 }
