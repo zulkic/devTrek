7c30658cb081b5d5c3c59e0fbd961f3d575d595c
Nicolas <negs.hunterx@gmail.com>
2015-06-10 14:16:27 -0300

    04-06-2015 volvio a funcionar el login, comiteando Globals

---
+++ app/src/main/java/com/mapas/franciscojavier/trekkingroute/Utility/Globals.java
@@ -0,0 +1,10 @@
+package com.mapas.franciscojavier.trekkingroute.Utility;
+
+/**
+ * Created by Josemar on 04-06-2015.
+ */
+public class Globals {
+
+    public static String EMAIL_REGEX = "\\b(?:[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*|\"(?:[\\x01-\\x08\\x0b\\x0c\\x0e-\\x1f\\x21\\x23-\\x5b\\x5d-\\x7f]|\\\\[\\x01-\\x09\\x0b\\x0c\\x0e-\\x7f])*\")@(?:(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?|\\[(?:(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\\.){3}(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?|[a-z0-9-]*[a-z0-9]:(?:[\\x01-\\x08\\x0b\\x0c\\x0e-\\x1f\\x21-\\x5a\\x53-\\x7f]|\\\\[\\x01-\\x09\\x0b\\x0c\\x0e-\\x7f])+)\\])\\b";
+    public static String PASSWORD_REGEX = "^(?=.*[A-Za-z])(?=.*\\d)[A-Za-z\\d]{6,}$";
+}
