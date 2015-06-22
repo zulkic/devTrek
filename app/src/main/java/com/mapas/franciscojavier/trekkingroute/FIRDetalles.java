9f158759b15523fad0f9d87288ca83918159c30c
Nicolas <negs.hunterx@gmail.com>
2015-06-22 00:10:33 -0300

    22-06-2015 Cambio a la barra lateral y agrego el planificar viaje

---
+++ app/src/main/java/com/mapas/franciscojavier/trekkingroute/FIRDetalles.java
@@ -0,0 +1,28 @@
+package com.mapas.franciscojavier.trekkingroute;
+
+import android.support.v4.app.Fragment;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+public class FIRDetalles extends Fragment {
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // TODO Auto-generated method stub
+        return inflater.inflate(R.layout.fir_layout_detalles, container,false);
+    }
+    public static FIRDetalles newInstance(String text) {
+
+        FIRDetalles f = new FIRDetalles();
+        Bundle b = new Bundle();
+        b.putString("msg", text);
+
+        f.setArguments(b);
+
+        return f;
+    }
+}
